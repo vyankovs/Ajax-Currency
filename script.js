3805494c5c65d1ab5dca12049aad6df5116b6148
vyankovs <viktoriia.yankovska@gmail.com>
2020-04-23 14:03:16 +0200

    no equal currencies and empty input

--- script.js
+++ script.js
@@ -9,29 +9,34 @@ const swap = document.getElementById("swap");
 function calculate() {
   const currency_one = currencyFrom.value;
   const currency_two = currencyTo.value;
+  if (currency_one == currency_two) {
+    alert("Currencies cannot match");
+  } else if (amountFrom.value == "") {
+    alert("Please insert amount you want to convert");
+  } else {
+    var xhr = new XMLHttpRequest();
 
-  var xhr = new XMLHttpRequest();
+    xhr.open(
+      "GET",
+      `https://api.exchangerate-api.com/v4/latest/${currency_one}`,
+      true
+    );
+    xhr.onload = function () {
+      if (this.status == 200) {
+        var response = JSON.parse(this.responseText);
 
-  xhr.open(
-    "GET",
-    `https://api.exchangerate-api.com/v4/latest/${currency_one}`,
-    true
-  );
-  xhr.onload = function () {
-    if (this.status == 200) {
-      var response = JSON.parse(this.responseText);
+        const rate = response.rates[currency_two];
 
-      const rate = response.rates[currency_two];
+        rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
 
-      rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
+        amountTo.value = (amountFrom.value * rate).toFixed(2);
+      } else {
+        console.log("fail");
+      }
+    };
 
-      amountTo.value = (amountFrom.value * rate).toFixed(2);
-    } else {
-      console.log("fail");
-    }
-  };
-
-  xhr.send();
+    xhr.send();
+  }
 }
 
 currencyFrom.addEventListener("change", calculate);
